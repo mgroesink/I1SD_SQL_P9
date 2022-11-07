0737f41c000dd12dd55c798ab21d4b8fd0a24408
Marcel Roesink <mroesink@rocvantwente.nl>
2022-11-07 13:49:42 +0100

    Create uitwerking.sql
    Basic template for solution

---
+++ uitwerking.sql
@@ -0,0 +1,21 @@
+/*
+Toets SQL sectie 1 t/m 8
+  Naam: 
+  Klas: 
+  Versie: 
+*/
+
+-- Opdracht 1
+
+-- Opdracht 2
+
+-- Opdracht 3
+
+-- Opdracht 4
+
+-- Opdracht 5
+
+-- Opdracht 6
+
+-- Opdracht 7 (bonus)
+
